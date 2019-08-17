7a30ad98791cd4f482c69817d5f68ecde3b32339
泰輔 服部 <taisuke.hattori@asno-sys.co.jp>
2019-08-18 07:52:08 +0900

    フロント側修正　WebAPI呼び出し処理追加

--- WebApplication2/WebApplication2/WebForm1.aspx.cs
+++ WebApplication2/WebApplication2/WebForm1.aspx.cs
@@ -6,50 +6,121 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Runtime.Serialization;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace WebApplication2
 {
     public partial class WebForm1 : System.Web.UI.Page
     {
+        [JsonObject( "kyain" )]
+        internal class KyainResponse
+        {
+            [JsonProperty( "prediction" )]
+            public decimal Prediction { get; set; }
+
+            [JsonProperty( "success" )]
+            public bool Success { get; set; }
+        }
+
         protected void Page_Load(object sender, EventArgs e)
         {
 
         }
+
         protected void JudgeButton_Click1(object sender, EventArgs e)
         {
             HttpPostedFile posted = Request.Files["FileUpload"];
 
             if (posted.FileName != "")
             {
-                String Value = ConfigurationManager.AppSettings["URL"];
-                string path = MapPath("./");
+                String directoryPath = ConfigurationManager.AppSettings[ "RelativeDirectoryPath" ];
+                string rootPath = MapPath("./");
                 posted.SaveAs(
-                //"c:\\UploadedFiles\\"
-                path + Value //Webサーバを指定
-                + System.IO.Path.GetFileName(posted.FileName));
-
-                //String OriginalScore = Deserialize<string>(json);
-                //Decimal CalculationScore = Convert.ToDecimal(OriginalScore);
-                //String Score = Convert.ToString(Math.Floor(CalculationScore * 100));
-                //ScoreBoX.Text = Score + "点";
+                    rootPath + directoryPath //Webサーバを指定
+                    + System.IO.Path.GetFileName( posted.FileName )
+                );
+                
+                var score = GetScore( posted.FileName );
+                ScoreBox.Text = Math.Floor( score * 100 ).ToString();
             }
-            String json = test();
-            String OriginalScore = Deserialize<string>(json);
-            Decimal CalculationScore = Convert.ToDecimal(OriginalScore);
-            String Score = Convert.ToString(Math.Floor(CalculationScore * 100));
-            ScoreBox.Text = Score + "点";
+            else
+            {
+                ScoreBox.Text = "ファイルを選択してください。";
+            }
         }
-        public T Deserialize<T>(string json)
+
+        /// <summary>
+        /// スコアを取得する
+        /// </summary>
+        /// <param name="fileName">評価対象のファイル名</param>
+        /// <returns></returns>
+        private decimal GetScore(string fileName )
+        {
+            if( ConfigurationManager.AppSettings[ "UsingStub" ].Equals(bool.TrueString) )
+            {
+                return (decimal)0.987654321;
+            }
+
+            using( HttpClient client = new HttpClient() )
+            {
+                string endpointRoot = ConfigurationManager.AppSettings[ "EndpointRoot" ];
+
+                // API呼び出し
+                client.BaseAddress = new Uri( endpointRoot );
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add( new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue( "application/json" ) );
+                
+                HttpResponseMessage response = client.GetAsync( "predict" ).Result;
+                
+                if( response.IsSuccessStatusCode )
+                {
+                    var resJson = response.Content.ReadAsStringAsync().Result;
+                    var kyainResponse = JsonConvert.DeserializeObject<KyainResponse>( resJson );
+                    response.Dispose();
+                    return kyainResponse.Prediction;
+                }
+                else
+                {
+                    // API呼び出し失敗時は -1 返す
+                    response.Dispose();
+                    return -1;
+                }
+
+                //// ポストする場合
+                //JsonUser newData = new JsonUser
+                //{
+                //    ID = -1,
+                //    Name = "New Data",
+                //    Email = "hello@test.jp",
+                //    Telephone = "111-222-333"
+                //};
+                //response = client.PostAsJsonAsync( "api/user", newData ).Result;
+                //if( response.IsSuccessStatusCode )
+                //{
+                //    Label1.Text = response.Headers.Location.ToString();
+                //}
+                //else
+                //{
+                //    ErrorLabel1.Text = response.ReasonPhrase;
+                //}
+                //response.Dispose();
+            }
+        }
+        public T Deserialize<T>( string json )
         {
             T result;
 
             System.Runtime.Serialization.Json.DataContractJsonSerializer serializer
-                        = new System.Runtime.Serialization.Json.DataContractJsonSerializer(typeof(T));
+                        = new System.Runtime.Serialization.Json.DataContractJsonSerializer( typeof( T ) );
 
-            using (System.IO.MemoryStream stream
-                = new System.IO.MemoryStream(System.Text.Encoding.UTF8.GetBytes(json)))
+            using( System.IO.MemoryStream stream
+                = new System.IO.MemoryStream( System.Text.Encoding.UTF8.GetBytes( json ) ) )
             {
-                result = (T)serializer.ReadObject(stream);
+                result = ( T )serializer.ReadObject( stream );
             }
 
             return result;
