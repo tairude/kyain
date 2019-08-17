db2d09f6109f673b55e08432dfbe500cec87fb22
sato <sato@A190412NOS>
2019-08-17 17:54:19 +0900

    Amano saisin

--- WebApplication2/WebApplication2/WebForm1.aspx.cs
+++ WebApplication2/WebApplication2/WebForm1.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -16,38 +17,49 @@ namespace WebApplication2
         }
         protected void JudgeButton_Click1(object sender, EventArgs e)
         {
-            //HttpPostedFile posted = Request.Files["FileUpload"];
+            HttpPostedFile posted = Request.Files["FileUpload"];
 
-            //if (posted.FileName != "")
-            //{
-            //    posted.SaveAs(
-            //    //"c:\\UploadedFiles\\"
-            //    "c:\\Users\\sato\\Desktop\\" //Webサーバを指定
-            //      + System.IO.Path.GetFileName(posted.FileName));
+            if (posted.FileName != "")
+            {
+                String Value = ConfigurationManager.AppSettings["URL"];
+                string path = MapPath("./");
+                posted.SaveAs(
+                //"c:\\UploadedFiles\\"
+                path + Value //Webサーバを指定
+                + System.IO.Path.GetFileName(posted.FileName));
 
-            //    String OriginalScore = Deserialize<string>(json);
-            //    Decimal CalculationScore = Convert.ToDecimal(OriginalScore);
-            //String Score = Convert.ToString(Math.Floor(CalculationScore * 100));
-            //    ScoreBoX.Text = Score + "点";
-            //}
-            String Score = Convert.ToString(Math.Floor(0.0999888888888888* 100));
+                //String OriginalScore = Deserialize<string>(json);
+                //Decimal CalculationScore = Convert.ToDecimal(OriginalScore);
+                //String Score = Convert.ToString(Math.Floor(CalculationScore * 100));
+                //ScoreBoX.Text = Score + "点";
+            }
+            String json = test();
+            String OriginalScore = Deserialize<string>(json);
+            Decimal CalculationScore = Convert.ToDecimal(OriginalScore);
+            String Score = Convert.ToString(Math.Floor(CalculationScore * 100));
             ScoreBox.Text = Score + "点";
         }
-        //public T Deserialize<T>(string json)
-        //{
-        //    T result;
+        public T Deserialize<T>(string json)
+        {
+            T result;
 
-        //    System.Runtime.Serialization.Json.DataContractJsonSerializer serializer
-        //                = new System.Runtime.Serialization.Json.DataContractJsonSerializer(typeof(T));
+            System.Runtime.Serialization.Json.DataContractJsonSerializer serializer
+                        = new System.Runtime.Serialization.Json.DataContractJsonSerializer(typeof(T));
 
-        //    using (System.IO.MemoryStream stream
-        //        = new System.IO.MemoryStream(System.Text.Encoding.UTF8.GetBytes(json)))
-        //    {
-        //        result = (T)serializer.ReadObject(stream);
-        //    }
+            using (System.IO.MemoryStream stream
+                = new System.IO.MemoryStream(System.Text.Encoding.UTF8.GetBytes(json)))
+            {
+                result = (T)serializer.ReadObject(stream);
+            }
 
-        //    return result;
+            return result;
 
-        //}
+        }
+
+        private string test()
+        {
+            return "0.01";
+        }
+
     }
 }
