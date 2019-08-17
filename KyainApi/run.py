872376d2b2a47af0187e22264bdeecdfdd68a409
yoshikisser <yoshikisser@users.noreply.github.com>
2019-08-17 13:03:03 +0900

    修正

--- KyainApi/run.py
+++ KyainApi/run.py
@@ -20,32 +20,25 @@ logger.addHandler(ch)
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='tf-pose-estimation run')
-    parser.add_argument('--image', type=str, default='./images/p1.jpg')
-    parser.add_argument('--model', type=str, default='cmu',
-                        help='cmu / mobilenet_thin / mobilenet_v2_large / mobilenet_v2_small')
-    parser.add_argument('--resize', type=str, default='0x0',
-                        help='if provided, resize images before they are processed. '
-                             'default=0x0, Recommends : 432x368 or 656x368 or 1312x736 ')
-    parser.add_argument('--resize-out-ratio', type=float, default=4.0,
-                        help='if provided, resize heatmaps before they are post-processed. default=1.0')
+    config = {
+        'image':'./images/kyain.jpg',
+        'model':'mobilenet_thin',
+        'resize':'432x368',
+        'resize_out_ratio': 4.0,
+    }
 
-    args = parser.parse_args()
-
-    w, h = model_wh(args.resize)
-    if w == 0 or h == 0:
-        e = TfPoseEstimator(get_graph_path(args.model), target_size=(432, 368))
-    else:
-        e = TfPoseEstimator(get_graph_path(args.model), target_size=(w, h))
+    w, h = model_wh(config['resize'])
+    e = TfPoseEstimator(get_graph_path(config['model']), target_size=(w, h)) 
 
     # estimate human poses from a single image !
-    image = common.read_imgfile(args.image, None, None)
+    image = common.read_imgfile(config['image'], None, None)
+
     if image is None:
-        logger.error('Image can not be read, path=%s' % args.image)
+        logger.error('Image can not be read, path=%s' % config['image'])
         sys.exit(-1)
 
     t = time.time()
-    humans = e.inference(image, resize_to_default=(w > 0 and h > 0), upsample_size=args.resize_out_ratio)
+    humans = e.inference(image, resize_to_default=(w > 0 and h > 0), upsample_size=config['resize_out_ratio'])
     elapsed = time.time() - t
     human_pts_list = np.zeros((len(humans),18,2))
     image_h, image_w = image.shape[:2]
@@ -62,7 +55,7 @@ if __name__ == '__main__':
            
     print(human_pts_list)
 
-    logger.info('inference image: %s in %.4f seconds.' % (args.image, elapsed))
+    logger.info('inference image: %s in %.4f seconds.' % (config['image'], elapsed))
 
     image = TfPoseEstimator.draw_humans(image, humans, imgcopy=False)
 
