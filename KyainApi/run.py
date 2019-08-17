8bdac1cf8b0c6e895188f7cb9ae026b64ce761c6
yoshikisser <yoshikisser@users.noreply.github.com>
2019-08-17 15:33:18 +0900

    score出るように

--- KyainApi/run.py
+++ KyainApi/run.py
@@ -18,14 +18,19 @@ formatter = logging.Formatter('[%(asctime)s] [%(name)s] [%(levelname)s] %(messag
 ch.setFormatter(formatter)
 logger.addHandler(ch)
 
+def cos_sim(v1, v2):
+    return np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2))
+
 
 if __name__ == '__main__':
     config = {
-        'image':'./images/kyain.jpg',
+        'image':'./images/kyain_1_2.jpg',
         'model':'mobilenet_thin',
         'resize':'432x368',
         'resize_out_ratio': 4.0,
     }
+
+    vec_i = [(0,1),(4,3),(3,2),(2,5),(5,6),(6,7),(2,8),(5,11),(8,9),(9,10),(11,12),(12,13)]
 
     w, h = model_wh(config['resize'])
     e = TfPoseEstimator(get_graph_path(config['model']), target_size=(w, h)) 
@@ -46,14 +51,32 @@ if __name__ == '__main__':
         # draw point
         for i in range(common.CocoPart.Background.value):
             if i not in human.body_parts.keys():
-                center = -1,-1
+                body_part.x = 0
+                body_part.y = 0
+            else:
+                body_part = human.body_parts[i]
 
-            body_part = human.body_parts[i]
             center = (int(body_part.x * image_w + 0.5), int(body_part.y * image_h + 0.5))
             human_pts_list[hid,i,0] = center[0]
             human_pts_list[hid,i,1] = center[1]
            
     print(human_pts_list)
+
+    vec_list = np.zeros((len(humans),12,2))
+    for hid in range(len(humans)):
+        for (v,i)in zip(vec_i,range(12)):
+            vec_list[hid,i,0] = human_pts_list[hid,v[0],0] - human_pts_list[hid,v[1],0]
+            vec_list[hid,i,1] = human_pts_list[hid,v[0],1] - human_pts_list[hid,v[1],1]
+
+
+    print(vec_list)
+
+    cos_list = np.zeros(12)
+    for i in range(12):
+        cos_list[i] = cos_sim(vec_list[0,i],vec_list[1,i])
+        
+    print(cos_list)
+    print('score:',np.mean(cos_list))
 
     logger.info('inference image: %s in %.4f seconds.' % (config['image'], elapsed))
 
